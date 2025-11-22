9116996c60e24c8b98f2c2d746be0bc09f208f99
babakarbab <147725789+babakarbab@users.noreply.github.com>
2025-11-22 14:31:10 +0100

    Add files via upload

---
+++ Washing Mashine Model/WMmodel_ini.m
@@ -0,0 +1,25 @@
+
+outerDrumLiqLevel_ini=0.1; 
+outerDrumArea=0.2;         
+outerDrumLen=1;            
+outerDrumMaxFill=50;       
+outletPipeEffLen=1;  
+outletPipeDia=0.025; 
+inletPipeEffLen=1;  
+inletPipeDia=0.025;  
+valveSpoolPosMax=0.01; 
+valveSpoolTravel=9e-3; 
+valveFaultTime=920;    
+motorPhase=1;            
+motorNumPolePair=2;      
+motorRatedMechPower=150; 
+motorRatedSpeed=1250;    
+motorRatedRMScurr=1.2;   
+motorEff=80;             
+motorRotorInertia=1e-3;  
+motorRotorDamping=1e-4;  
+pumpCapacity=[380 580 850 1110 1370 1640 1910 2170 2450]/60; 
+pumpHead=[30 27 24 21 18 15 12 9 6];                         
+pumpBrakePower=[30 27 24 21 18 15 12 9 6]/20;                
+pumpShaftSpeed=3000;                                         
+
