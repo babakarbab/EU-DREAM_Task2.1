083906453f39e96d9eb4e0a1a17d752854542ba4
babakarbab <147725789+babakarbab@users.noreply.github.com>
2025-11-22 14:41:17 +0100

    Add files via upload

---
+++ Dishwasher Model/DWmodel_ini.m
@@ -0,0 +1,24 @@
+
+
+ambient=298;            
+basinArea=0.2;          
+machineHeight=1.5;      
+iniBasinLiqLevel=1e-3;   
+maxBasinLiqTemp=350;    
+minBasinLiqTemp=305;    
+maxBasinLiqLevel=0.10;  
+minBasinLiqLevel=0.04;  
+lpmLeakageFlow=0.001;  
+motorPower=500;         
+motorSpeed=500;         
+heaterPower=1800;       
+powerWashCycle=500;     
+outletPipeEffLen=2;  
+outletPipeDia=0.025; 
+inletPipeEffLen=2;   
+inletPipeDia=0.025;  
+pumpOutletDia=0.025; 
+pumpCapacity=[380 580 850 1110 1370 1640 1910 2170 2450]/60; 
+pumpHead=[30 27 24 21 18 15 12 9 6];                         
+pumpBrakePower=[30 27 24 21 18 15 12 9 6]/20;                
+pumpShaftSpeed=3000;                                         
